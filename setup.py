ec2110e4ef401fba2a8d25cc36830a49e8e0a4a9
Shivansh <singh.shivansh2410@gmail.com>
2023-09-17 14:13:07 +0530

    first setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function returns list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+setup(
+    name='question-answering-system',
+    version='0.0.1',
+    author='Shivansh',
+    author_email='singh.shivansh2410@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
