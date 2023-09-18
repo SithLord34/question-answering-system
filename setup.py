a5fb933c6bb77aa10fe2b7be93bcfc43646bf5cf
Shivansh <singh.shivansh2410@gmail.com>
2023-09-18 23:31:19 +0530

    hyphen_e_dot install packages in editable mode

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import find_packages,setup
 from typing import List
 
+HYPHEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
     this function returns list of requirements
@@ -9,6 +10,10 @@ def get_requirements(file_path:str)->List[str]:
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
 
 setup(
     name='question-answering-system',
