fa07a6fdfc3f861f3d58d865bc811a04f5376de5
Shivansh <singh.shivansh2410@gmail.com>
2023-09-23 00:12:51 +0530

    exception, import logger as module correction

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from src.logger import logging
+from logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -16,10 +16,3 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-    
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Division by zero")
-        raise CustomException(e,sys)
